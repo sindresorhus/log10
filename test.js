08fe41234433645b1c0c2375dddbc09f14f173e0
Sindre Sorhus <sindresorhus@gmail.com>
2016-01-01 00:32:45 +0100

    update tests for latest AVA version

--- test.js
+++ test.js
@@ -1,22 +1,21 @@
-'use strict';
-var test = require('ava');
-var numberIsNan = require('number-is-nan');
-var positiveZero = require('positive-zero');
-var floatEqual = require('float-equal');
-Math.log10 = undefined;
-var log10 = require('./');
+import test from 'ava';
+import numberIsNan from 'number-is-nan';
+import positiveZero from 'positive-zero';
+import floatEqual from 'float-equal';
 
-test(function (t) {
-	t.assert(numberIsNan(log10(NaN)));
-	t.assert(numberIsNan(log10(-1e-50)));
-	t.assert(positiveZero(log10(1)));
-	t.assert(log10(0) === -Infinity);
-	t.assert(log10(-0) === -Infinity);
-	t.assert(log10(Infinity) === Infinity);
-	t.assert(floatEqual(log10(0.1), -1));
-	t.assert(floatEqual(log10(0.5), -0.3010299956639812));
-	t.assert(floatEqual(log10(1.5), 0.17609125905568124));
-	t.assert(floatEqual(log10(5), 0.6989700043360189));
-	t.assert(floatEqual(log10(50), 1.6989700043360187));
-	t.end();
+Math.log10 = undefined;
+const m = require('./');
+
+test(t => {
+	t.true(numberIsNan(m(NaN)));
+	t.true(numberIsNan(m(-1e-50)));
+	t.true(positiveZero(m(1)));
+	t.is(m(0), -Infinity);
+	t.is(m(-0), -Infinity);
+	t.is(m(Infinity), Infinity);
+	t.true(floatEqual(m(0.1), -1));
+	t.true(floatEqual(m(0.5), -0.3010299956639812));
+	t.true(floatEqual(m(1.5), 0.17609125905568124));
+	t.true(floatEqual(m(5), 0.6989700043360189));
+	t.true(floatEqual(m(50), 1.6989700043360187));
 });
